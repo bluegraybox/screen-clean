d34db81e11044ddb8fc1704b6c4d6c892e48493a
Colin MacDonald <colin.r.macdonald@gmail.com>
2018-12-24 03:30:36 -0500

    Tidy up Facebook CSS & JS
    Removed commented-out code, improve actual comments.

--- facebook.js
+++ facebook.js
@@ -1,36 +1,25 @@
 function hideComments(oldComments) {
-    /*
-    console.log("oldComments:");
-    for (var i = 0; i < oldComments.length; i++) {
-        console.log(oldComments[i]);
-    }
-    */
-    // comments is an HTMLCollection; allComments is an array
+    // comments is an HTMLCollection; oldComments and allComments are arrays
     var comments = document.getElementsByClassName("UFIContainer");
     var newComments = [];
     var allComments = [];
-    // console.log("comments:");
     for (var i = 0; i < comments.length; i++) {
         var c = comments.item(i);
-        // console.log(c);
         allComments.push(c);
         if (!oldComments.includes(c)) {
             newComments.push(c);
         }
     }
-    // console.log("Inserting elements");
     for (var i = 0; i < newComments.length; i++) {
-        // console.log("Inserting element " + i);
         console.log("New element " + newComments[i].id);
-        // var t = '<div class="showComments">Show Comments</div>';
         var button = document.createElement("div");
         button.classList.add("showComments");
         button.innerHTML = "Show/Hide Comments";
         button.addEventListener("click", showFunc(newComments[i]));
         newComments[i].insertAdjacentElement("beforebegin", button);
-        // newComments[i].insertAdjacentHTML("beforebegin", t);
         newComments[i].style.display = "none";
     }
+    // pass the new allComments to the next invocation
     setTimeout(() => hideComments(allComments), 1000);
 }
 
@@ -45,10 +34,5 @@ function showFunc(commentBlock) {
     };
 }
 
+// Start with an empty array
 hideComments([]);
-
-/*
-document.addEventListener("load", function(event) {
-    hideComments();
-});
-*/
