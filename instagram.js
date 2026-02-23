03a0ea8bc2300f13f4608fa5b2acf658209c2233
Colin MacDonald <colin.r.macdonald@gmail.com>
2026-02-23 17:51:43 -0500

    Improve Instagram ad sniping

--- instagram.js
+++ instagram.js
@@ -1,15 +1,38 @@
-function hideAds() {
-    var sponsoredTags = document.getElementsByClassName("x1fhwpqd x132q4wb x5n08af");
-    for (let i = 0; i < sponsoredTags.length; i++) {
-        sponsoredTags.item(i).parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.innerHTML = '';
-        console.log("hiding ad");
+function blankArticle(article, message) {
+    return function () {
+        // article.innerHTML = '';
+        console.log(message);
     }
+}
 
+function hideAds() {
+    /*
+    var sponsoredTags = document.getElementsByClassName("x1fhwpqd x132q4wb x5n08af");  // this feels fragile and unreliable
+    for (let i = 0; i < sponsoredTags.length; i++) {
+        var item = sponsoredTags.item(i).parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+        setTimeout(blankArticle(item, "hiding ad " + i), 1000);
+    }
+    */
+
+    // blank out articles that contain any of these phrases
+    var killPhrases = ['Learn more', 'Suggested for you', 'Shop now', 'Order now', 'Watch more'];
+    var articles = document.getElementsByTagName("article");
+    for (let i = 0; i < articles.length; i++) {
+        var item = articles.item(i);
+        for (let j = 0; j < killPhrases.length; j++) {
+            if (item.innerHTML.includes(killPhrases[j])) {
+                // do this on a delay so I have the satisfaction of watching it get deleted
+                setTimeout(blankArticle(item, "hiding article " + i), 2000);
+                continue;
+            }
+        }
+    }
 }
 
 function poll() {
     hideAds();
-    setTimeout(poll, 2000);  // run every 2 seconds
+    var delay = 1;  // run every N seconds
+    setTimeout(poll, delay * 1000);
 }
 
 poll();
